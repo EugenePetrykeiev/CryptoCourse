3be06eb6dfe02cca2adf52c82838a62e1fcd22a9
EugenePetrykeiev <eugenepetrikeev@gmail.com>
2022-06-20 03:46:39 +0300

    delete files

--- Day4/tests/aesalter.py
+++
@@ -1,5 +0,0 @@
-import aes128, os
-key = os.urandom(16)
-iv = os.urandom(16)
-encrypted = aes.AES(key).encrypt_ctr(b'Attack at dawn', iv)
-print(aes.AES(key).decrypt_ctr(encrypted, iv))
--- Day4/tests/main.py
+++
@@ -1,6 +0,0 @@
-import test
-
-cipher = test.encrypt(b'input_bytes', 'ZZHHYYTTUUHHGR')
-print(cipher)
-
-
