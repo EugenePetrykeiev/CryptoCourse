daf9649f56628156cf0a926b65108ab7c5ac29a1
EugenePetrykeiev <eugenepetrikeev@gmail.com>
2022-06-20 23:41:54 +0300

    edited readme.md. Minor fixes

--- Day4/main.py
+++ Day4/main.py
@@ -1,10 +1,9 @@
 from aes128 import aes_encryption
 import base64
 
-cipher = aes_encryption('Lorem ipsum dolor', '1234567890abcdef')
+cipher = aes_encryption('123456789012345', '1234567890abcdef')
 message_bytes = cipher.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 base64_message = base64_bytes.decode('ascii')
 print('Cipher: ', cipher)
 print('Base 64: ', base64_message)
-print(bytearray([1]))
